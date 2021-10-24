c4e5be0b3ba6586216786e4dba07d5cea0e1dbb4
sojin <325days@naver.com>
2021-10-25 03:24:21 +0900

    Add Rendering the Movies

---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import propTypes from "prop-types";
+
+function Movie({id, year, title, summary, poster}){
+    return <h1>{title}</h1>
+
+}
+
+
+Movie.propTypes = {
+    id : propTypes.number.isRequired,
+    year : propTypes.number.isRequired,
+    title : propTypes.string.isRequired,
+    summary : propTypes.string.isRequired,
+    poster : propTypes.string.isRequired
+}
+
+export default Movie;
